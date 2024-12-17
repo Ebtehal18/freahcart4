29acd2504549a8f74197e936ba2ce0d97367d241
Ebtehal18 <ebtehallelsayed18@gmail.com>
2024-12-17 13:58:58 +0200

    modify search bar

--- .history/src/components/RecentProuducts/RecentProuducts_20241217135818.jsx
+++ .history/src/components/RecentProuducts/RecentProuducts_20241217135818.jsx
@@ -19,7 +19,7 @@ if(isError){
 }
 
   return <>
- <div className='px-4 lg:px-0'>
+ <div>
  <input value={search} onChange={(e)=>setSearch(e.target.value)} type="text" id="helper-text" aria-describedby="helper-text-explanation" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mt-12   dark:focus:ring-orange-500 dark:focus:border-orange-500 " placeholder="search..."></input>
  </div>
 <div className='grid lg:grid-cols-6 md:grid-cols-4 sm:grid-cols-2 gap-5 py-12 px-4 '>
